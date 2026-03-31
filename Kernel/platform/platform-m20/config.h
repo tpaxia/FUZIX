17bf711c80700690d3b1c1d8d5490f46df6d5952
tpaxia <spaxia@notoslab.com>
2026-03-31 12:15:34 -0400

    z8000: rw filesystem, boot messages on CRT, proper diskimage build
    - Mount root filesystem read-write (CMDLINE "rw" instead of NULL)
    - _outchar now writes to both serial and CRT BIOS, so kernel boot
      messages appear on the M20 screen as well as the debug serial port
    - diskimage target uses build-filesystem-ng with standard .pkg files
      for a full filesystem (100+ binaries, /etc configs, symlinks)
    - Full init (login/getty) hangs after motd; using stub init for now

--- Kernel/platform/platform-m20/config.h
+++ Kernel/platform/platform-m20/config.h
@@ -40,7 +40,7 @@
 
 #define BOOT_TTY (512 + 1)	/* First TTY device (serial port) */
 
-#define CMDLINE	NULL
+#define CMDLINE	"rw"
 
 /* Device parameters */
 #define NUM_DEV_TTY 1		/* CRT display + keyboard */
